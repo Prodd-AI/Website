cd7b1ddda143c4115366f28095859469377268c0
Saviour Ise <saviourise1@gmail.com>
2025-06-28 23:54:03 +0100

    hero section responsiveness

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -5,7 +5,7 @@ import heroBg from "../../assets/images/hero-bg.png";
 const Hero = () => {
   return (
     <div
-      className="flex flex-col items-center justify-center h-[40rem] gap-6 relative"
+      className="flex flex-col items-center justify-center h-[40rem] gap-6 relative px-6 sm:px-4"
       style={{
         backgroundImage: `url(${heroBg})`,
         backgroundSize: "cover",
@@ -14,8 +14,8 @@ const Hero = () => {
       }}
     >
       <div className="absolute inset-0 bg-gradient-to-b from-black via-black/70 to-black"></div>
-      <div className="relative z-10 flex flex-col items-center justify-center gap-6">
-        <h1 className="text-[80px] font-semibold text-center flex flex-col items-center justify-center leading-26">
+      <div className="relative z-10 flex flex-col sm:items-center justify-center gap-6">
+        <h1 className="text-[40px] md:text-[80px] font-semibold text-left sm:text-center flex flex-col items-center justify-center leading-12 md:leading-26">
           <span className="bg-gradient-to-b from-white to-gray-400 bg-clip-text text-transparent">
             The AI Operating System
           </span>
@@ -24,13 +24,13 @@ const Hero = () => {
           </span>
         </h1>
 
-        <p className="text-white font-normal text-base text-center max-w-[600px]">
+        <p className="text-white font-normal text-base sm:text-center text-left max-w-[600px]">
           Empower your workforce with intelligent agents that amplify
           productivity, detect burnout early, and elevate team culture before
           it's too late.
         </p>
 
-        <div className="flex items-center justify-center gap-4 mt-4">
+        <div className="flex sm:items-center items-start justify-center gap-4 mt-4 flex-col sm:flex-row">
           <GradientButton>
             <span className="text-white font-kumbh-semibold text-sm">
               See ProdAI in action
